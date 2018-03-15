5511a0f6232e0d29ada503d70d997a22ffb2b1c8
Unnsin <puypuy@ukr.net>
2018-03-15 15:15:12 +0200

    second commit

--- twitterpars.py
+++ twitterpars.py
@@ -2,7 +2,7 @@
 # developer: Lesnoy V.S.
 # Date:
 # Time:
-# Documentation: 
+# Documentation:
 # ----------------------------------------------
 import urllib.request
 from bs4 import BeautifulSoup
@@ -10,6 +10,11 @@ import sys
 filename = "sysname.txt"
 file = open(filename, encoding="UTF-8", mode="r")
 
+class tweet:
+    def __init__(self, name, text, url):
+        self.Text = text;
+        self.Name = name;
+        self.URL = url;
 
 def get_html(url):
     response = urllib.request.urlopen(url);
@@ -17,15 +22,24 @@ def get_html(url):
 
 def parse(html):
     soup = BeautifulSoup(html, "html.parser")
-    tweet = soup.find("p", class_="TweetTextSize TweetTextSize--normal js-tweet-text tweet-text")
+    tweetx = soup.find("p", class_="TweetTextSize TweetTextSize--normal js-tweet-text tweet-text")
     name = soup.find("span", class_="username u-dir u-textTruncate")
-    print("UserName: " + name.text)
-    print("Tweet: " + tweet.text)
+    tweety = tweet(name.text,tweetx.text,url)
+    return tweety
 
-
+tweets = [];
 for line in file:
     url = line
-    print(parse(get_html(url)))
-
-
-
+    buf = parse(get_html(url))
+    print("UserName: " + buf.Name)
+    print("Tweet: " + buf.Text)
+    tweets.append(buf)
+    print("\n")
+while(True):
+    for x in tweets:
+        buf = parse(get_html(x.URL))
+        if (buf.Text != x.Text):
+            print("UserName: "+ buf.Name)
+            print("Tweet: "+ buf.Text)
+            print("\n")
+            x.Text=buf.Text
